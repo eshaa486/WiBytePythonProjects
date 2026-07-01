a887b44cda5a3e0ef968f3cb4e7ffa06bdd4ea4a
esha <uniesha8@gmail.com>
2026-07-01 23:10:49 +0000

    save changes almost done

--- NumberHunt.py
+++ NumberHunt.py
@@ -1,11 +1,19 @@
 import random
 
-print('Choose difficulty(Easy or Hard): ')
+print('Choose difficulty(Easy, Medium, or Hard): ')
 diff = input()
-n = random.randint(1,50)
 
 if diff == "Easy":
-print('I have picked a number between 1-50. Can you guess it?')
+    n = random.randint(1,50)
+    print('I have picked a number between 1-50. Can you guess it?')
+
+if diff == "Medium":
+    n = random.randint(1,100)
+    print('I have picked a number between 1-100. Can you guess it?')
+
+if diff == "Hard":
+    n = random.randint(1,250)
+    print('I have picked a number between 1-250. Can you guess it?')
 
 
 attempts = 0
@@ -24,3 +32,41 @@ while not done:
         print('You have guessed the number correctly!')
         print('You took', attempts, 'attempts to guess this.\n')
         done = True
+
+
+#Now user selects the number
+
+
+print()
+print()
+
+print('Now your turn. You pick a number between 1-100')
+print('Click ENTER when ready.')
+
+input()
+done = False
+attempts = 0 
+guess = random.randint(1,100)
+guess_step = 10
+prev_answer = answer
+
+while not done:
+    answer = input('Is it ' + str(guess) + '? (y = yes, l = less than that, g = greater than that)\n')
+    attempts = attempts + 1
+
+    if answer == 'l':
+        guess = guess - guess_step
+        if guess < 1:
+            guess = 1
+
+    if answer == 'g':
+        guess = guess + guess_step
+        if guess > 100:
+            guess = 100
+
+    if answer == 'y':
+        print('Yes, I got it correct!')
+        print('I took ', attempts, 'attepts to guess this.')
+        done = True
+
+
